11c42a330382b224cac19a229470b278c9142e84
Nachthirsch <revengecounter202@gmail.com>
2024-08-21 21:42:21 +0700

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Header from "./header";
+import Form from "./Form";
+import Grocery from "./Grocery";
+import Footer from "./Footer";
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+
+export default function App() {
+  const [items, setItems] = useState(groceryItems);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleTogleItem(id) {
+    setItems((items) => items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item)));
+  }
+
+  function handleClearItems() {
+    setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      <Form onAddItem={handleAddItem} />
+      <Grocery items={items} onDeleteItem={handleDeleteItem} onTogleItem={handleTogleItem} onClearItems={handleClearItems} />
+      <Footer items={items} />
+    </div>
+  );
+}
